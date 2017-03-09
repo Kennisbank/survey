d7fae560df51af47810d9f03d16bffe268c6243c
Oualid <oualids1007@gmail.com>
2017-03-09 14:38:23 +0100

    Connectie met database coderen

--- Model/model.php
+++ Model/model.php
@@ -1,4 +1,16 @@
 <?php
 
+$servername = "rdbms.strato.de";
+$username = "U2882885";
+$password = "KYGV85gbj9";
+
+// Connectie met database
+$conn = new mysqli($servername, $username, $password);
+
+// Connectie met database testen
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 ?>
